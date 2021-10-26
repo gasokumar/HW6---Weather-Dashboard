9248c72528810532008566e92c1b18458a003339
Ami Asokumar <gasokumar@gmail.com>
2021-10-26 08:09:06 -0400

    Added API Key as well as information section tags

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,2 +1,15 @@
-// Dependencies
-var jumbotron = $(".jumbotron");
+//  DEPENDNCIES ===================================
+var citySearch = $("#city-search");
+var searchButton = $("#search-button");
+
+const APIKey = "00a458a5a4ee8e7d62edeb1913c60d1a";
+
+// FUNCTIONS ===============================
+// Store search in local storage
+// Fetch API Data using form/after button click
+// Output API Data
+
+// Current and future conditions for that city
+
+//City name, the date, temp, humidity, wind speed, and UV index
+// Same thing 6 times except in future conditions there's no UV or city name
